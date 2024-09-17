747a132b0e77b6d995a858ac3fe9ffabdf960112
ilcavero <ilcavero@gmail.com>
2024-09-17 05:20:28 +0200

    fixed input min/max attr and css

--- src/main/scala/ilcavero/Main.scala
+++ src/main/scala/ilcavero/Main.scala
@@ -47,13 +47,13 @@ object Model {
   def apply(size: Int): Model = Model((0 until size).map(_.toString))
 
   def resize(oldModel: Model, newMax: Int): Model = {
-    if(newMax > oldModel.array.size) {
-      Model(oldModel.array.appendedAll(( oldModel.array.size until newMax).map(_.toString)))
+    if (newMax > oldModel.array.size) {
+      Model(oldModel.array.appendedAll((oldModel.array.size until newMax).map(_.toString)))
     } else {
       var newArray = oldModel.array.take(newMax).map(_.toInt)
-      for(i <- newMax until oldModel.array.size) {
+      for (i <- newMax until oldModel.array.size) {
         val outValue = oldModel.array(i).toInt
-        if(outValue < newMax) {
+        if (outValue < newMax) {
           val (_, j) = newArray.zipWithIndex.find(x => x._1 >= newMax).get
           newArray = newArray.updated(j, outValue)
         }
@@ -103,18 +103,18 @@ object FactoradicDemo {
 
     def arrayUpdater(i: Int) = model.updater[String]((m, n) => if n.toIntOption.exists(x => x >= 0 && x < m.array.length) then Model.updateArray(m, i, n) else m)
 
-    def factoradicDecimalUpdater = model.updater[String]{ (m,d) =>
+    def factoradicDecimalUpdater = model.updater[String] { (m, d) =>
       d.toLongOption match
         case Some(v) if v >= 0 && v <= m.factoradicMax => Model.updateDecimalFactoradic(m, v)
         case _ => m
     }
 
-    def lehmerUpdater(i: Int) = model.updater[String]((m, l) => l.toIntOption.filter(x => x >= 0 && x <= (m.maxIndex - i)).map(li => Model.updateLehmer(m,i,li)).getOrElse(m))
+    def lehmerUpdater(i: Int) = model.updater[String]((m, l) => l.toIntOption.filter(x => x >= 0 && x <= (m.maxIndex - i)).map(li => Model.updateLehmer(m, i, li)).getOrElse(m))
 
     div(
       className := "m-3",
       children <-- model.signal.map { (m: Model) =>
-        val cols = if(m.array.size > 5) "col-1" else "col-2"
+        val cols = if (m.array.size > 5) "col-1" else "col-2"
         List(
           div(
             className := "mb-3",
@@ -123,6 +123,8 @@ object FactoradicDemo {
               "The elements of an ordered set (for example letters) can be mapped to the first ",
               input(
                 typ := "number",
+                minAttr := "1",
+                maxAttr := "11",
                 value := m.array.length.toString,
                 onChange.mapToValue --> resizer
               ),
@@ -153,6 +155,8 @@ object FactoradicDemo {
                     input(
                       className := "w-100",
                       typ := "number",
+                      minAttr := "0",
+                      maxAttr := s"${m.maxIndex}",
                       value := m.array(i),
                       onChange.mapToValue --> arrayUpdater(i)
                     )
@@ -175,7 +179,7 @@ object FactoradicDemo {
                     val lehmerCodePoint = m.lehmer(i)
                     val explanation = letters.zipWithIndex.map { (letter, i) =>
                       val text = s"$letter → $i"
-                      p(className := "mb-1 text-monospace", if(i == lehmerCodePoint) mark(cls := "p-0", text) else text)
+                      p(className := "mb-1 text-monospace", if (i == lehmerCodePoint) mark(cls := "p-0", text) else text)
                     }
                     letters.remove(lehmerCodePoint)
                     div(
@@ -184,6 +188,8 @@ object FactoradicDemo {
                       input(
                         className := "w-100",
                         typ := "number",
+                        minAttr := "0",
+                        maxAttr := s"${m.maxIndex - i}",
                         value := lehmerCodePoint.toString,
                         onChange.mapToValue --> lehmerUpdater(i)
                       )
@@ -196,29 +202,30 @@ object FactoradicDemo {
           div(
             className := "mb-3",
             h1("Factoradic to decimal"),
-            p("Interpreting the Lehmer code in the ", a("factoradic number system", target := "_blank", href := "https://en.wikipedia.org/wiki/Factorial_number_system"), " uniquely maps the permutation to a single number. This number can be converted to a decimal representation if we multiply each position in by the factorial of the position index."),
+            p("Interpreting the Lehmer code in the ", a("factoradic number system", target := "_blank", href := "https://en.wikipedia.org/wiki/Factorial_number_system"), f" uniquely maps the permutation to a number between 0 and ${m.factoradicMax}%,d. This conversion is done by multiplying each position in the code by the factorial of the position index. Try changing this number to generate a new permutation."),
             div(
-                className := "container d-inline-block",
+              className := "container d-inline-block",
+              div(
+                className := "row align-items-center",
+                m.lehmer.zipWithIndex.map {
+                  case (x, i) =>
+                    div(
+                      className := s"$cols p-1 text-center",
+                      span(className := "text-monospace", s"$x × ${m.maxIndex - i}!"),
+                      span(className := "float-right", if (i == m.array.length - 1) "=" else "+")
+                    )
+                },
                 div(
-                  className := "row align-items-center",
-                  m.lehmer.zipWithIndex.map {
-                    case (x, i) =>
-                      div(
-                        className := s"$cols p-1 text-center",
-                        span(className := "text-monospace", s"$x × ${m.maxIndex - i}!"),
-                        span(className := "float-right", if(i == m.array.length -1) "=" else "+")
-                      )
-                  },
-                  div(
-                    className := "col-1 p-1 text-nowrap",
-                    input(
-                      typ := "number",
-                      value := m.factoradicResult.toString,
-                      onChange.mapToValue --> factoradicDecimalUpdater
-                    ),
-                    span(f" out of ${m.factoradicMax}%,d possible permutations (Try changing this number to generate a new permutation)")
+                  className := "col-1 p-1",
+                  input(
+                    typ := "number",
+                    minAttr := "0",
+                    maxAttr := s"${m.factoradicMax}",
+                    value := m.factoradicResult.toString,
+                    onChange.mapToValue --> factoradicDecimalUpdater
                   )
                 )
+              )
             ),
           ),
           div(
@@ -228,8 +235,7 @@ object FactoradicDemo {
             div(
               className := "container d-inline-block",
               div(
-                cls := "row align-items-end",
-                {
+                cls := "row align-items-end", {
                   var factoradic = m.factoradicResult
                   for (i <- 1 to m.array.length) yield {
                     val factoradicNextDiv = factoradic / i
@@ -263,7 +269,7 @@ object FactoradicDemo {
                   for (lehmerCodePoint <- m.lehmer) yield {
                     val explanation = letters.zipWithIndex.map { (letter, i) =>
                       val text = s"$i → $letter"
-                      p(className := "mb-1 text-monospace", if(i == lehmerCodePoint) mark(cls := "p-0",text) else text)
+                      p(className := "mb-1 text-monospace", if (i == lehmerCodePoint) mark(cls := "p-0", text) else text)
                     }
                     val letter = letters.remove(lehmerCodePoint)
                     div(
